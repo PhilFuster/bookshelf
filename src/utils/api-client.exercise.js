b7979ebe86e8e145fbe4904cbccef728682cf33b
philfuster <pafus23@protonmail.com>
2022-09-08 12:00:15 -0400

    finished 1st exercise for exercise 3

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,9 +1,25 @@
-function client(endpoint, customConfig = {}) {
+async function client(endpoint, customConfig = {}) {
   // 🐨 create the config you'll pass to window.fetch
   //    make the method default to "GET"
+  const fetchConfig = {
+    method: 'GET',
+    ...customConfig
+  }
   // 💰 if you're confused by this, that's fine. Scroll down to the bottom
   // and I've got some code there you can copy/paste.
   // 🐨 call window.fetch(fullURL, config) then handle the json response
+  try {
+    const response = await window
+      .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, fetchConfig)
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    }
+    return Promise.reject(data)
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error fetching data..')
+  }
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
 }
